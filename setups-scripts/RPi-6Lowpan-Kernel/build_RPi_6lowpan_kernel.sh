048b9fd8fe601a323e5b76cd0fc0d9be4bb0bb73
lightertu <ruitu.login@gmail.com>
2016-08-30 18:09:53 -0700

    changed script

--- setups-scripts/RPi-6Lowpan-Kernel/build_RPi_6lowpan_kernel.sh
+++ setups-scripts/RPi-6Lowpan-Kernel/build_RPi_6lowpan_kernel.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+export PATH=$PATH:~/SDKs/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin/
 cd linux-rpi
 CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm make zImage modules dtbs -j8
 CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm INSTALL_MOD_PATH=.mods make modules_install
