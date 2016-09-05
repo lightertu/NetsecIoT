b579ac48e1196691f824fa451ee3658ff1e4c620
Rui Tu <ruit@uoregon.edu>
2016-09-05 12:22:09 -0700

    added the path of compiler in the build script

--- setups/RPi-6Lowpan-Kernel/build.sh
+++ setups/RPi-6Lowpan-Kernel/build.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 cd linux-rpi
+export PATH=$PATH:~/SDKs/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin
 make CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm zImage -j8
 make CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm modules -j8
 make CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm dtbs -j8
