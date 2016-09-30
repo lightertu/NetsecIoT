99ed253616579623cddd5e832c7e79619134d5b7
lightertu <ruit@uoregon.edu>
2016-09-29 21:57:47 -0700

    corrected some errors in the build script

--- gnrc_networking/bin/samr21-xpro/cpu/lpm_arch.d
+++
@@ -1,8 +0,0 @@
-/home/rui/Projects/6Lowpan-testbed/gnrc_networking/bin/samr21-xpro/cpu/lpm_arch.o: \
- /home/rui/SDKs/RIOT/cpu/samd21/lpm_arch.c \
- /home/rui/Projects/6Lowpan-testbed/gnrc_networking/bin/samr21-xpro/riotbuild/riotbuild.h \
- /home/rui/SDKs/RIOT/core/include/arch/lpm_arch.h
-
-/home/rui/Projects/6Lowpan-testbed/gnrc_networking/bin/samr21-xpro/riotbuild/riotbuild.h:
-
-/home/rui/SDKs/RIOT/core/include/arch/lpm_arch.h:
