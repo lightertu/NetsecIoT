7878c0b5aa81636babada06a684be8bf86dcd353
Rui Tu <ruit@uoregon.edu>
2016-10-05 04:58:27 +0000

    some changes in makefile

---
+++ samr21-test/bin/samr21-xpro/cortexm_common/cortexm_init.d
@@ -0,0 +1,249 @@
+/home/rui/Projects/6Lowpan-testbed/samr21-test/bin/samr21-xpro/cortexm_common/cortexm_init.o: \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/cortexm_init.c \
+ /home/rui/Projects/6Lowpan-testbed/samr21-test/bin/samr21-xpro/riotbuild/riotbuild.h \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/include/cpu.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/5.4.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/types.h \
+ /usr/include/newlib/sys/stdio.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/cpu_conf.h \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/include/cpu_conf_common.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/samd21.h \
+ /usr/include/newlib/stdint.h /usr/include/newlib/sys/_intsup.h \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/include/core_cm0plus.h \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/include/core_cmInstr.h \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/include/cmsis_gcc.h \
+ /home/rui/SDKs/RIOT/cpu/cortexm_common/include/core_cmFunc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_ac.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_adc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_dmac.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_dsu.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_eic.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_evsys.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_gclk.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_mtb.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_nvmctrl.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_pac.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_pm.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_port.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_rfctrl.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_rtc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_sercom.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_sysctrl.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_tc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_tcc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_usb.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/component/component_wdt.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_ac.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_adc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_dmac.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_dsu.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_eic.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_evsys.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_gclk.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_mtb.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_nvmctrl.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pac0.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pac1.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pac2.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pm.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_port.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_rfctrl.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_rtc.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom0.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom1.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom2.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom3.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom4.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom5.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sysctrl.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tc3.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tc4.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tc5.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tcc0.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tcc1.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tcc2.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_usb.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_wdt.h \
+ /home/rui/SDKs/RIOT/cpu/samd21/include/pio/pio_samr21g18a.h \
+ /home/rui/SDKs/RIOT/core/include/irq.h \
+ /usr/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ /home/rui/SDKs/RIOT/core/include/arch/irq_arch.h
+
+/home/rui/Projects/6Lowpan-testbed/samr21-test/bin/samr21-xpro/riotbuild/riotbuild.h:
+
+/home/rui/SDKs/RIOT/cpu/cortexm_common/include/cpu.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/nano/newlib.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/5.4.1/include/stdarg.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/cpu_conf.h:
+
+/home/rui/SDKs/RIOT/cpu/cortexm_common/include/cpu_conf_common.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/samd21.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/home/rui/SDKs/RIOT/cpu/cortexm_common/include/core_cm0plus.h:
+
+/home/rui/SDKs/RIOT/cpu/cortexm_common/include/core_cmInstr.h:
+
+/home/rui/SDKs/RIOT/cpu/cortexm_common/include/cmsis_gcc.h:
+
+/home/rui/SDKs/RIOT/cpu/cortexm_common/include/core_cmFunc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_ac.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_adc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_dmac.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_dsu.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_eic.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_evsys.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_gclk.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_mtb.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_nvmctrl.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_pac.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_pm.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_port.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_rfctrl.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_rtc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_sercom.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_sysctrl.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_tc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_tcc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_usb.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/component/component_wdt.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_ac.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_adc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_dmac.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_dsu.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_eic.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_evsys.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_gclk.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_mtb.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_nvmctrl.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pac0.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pac1.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pac2.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_pm.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_port.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_rfctrl.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_rtc.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom0.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom1.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom2.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom3.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom4.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sercom5.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_sysctrl.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tc3.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tc4.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tc5.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tcc0.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tcc1.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_tcc2.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_usb.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/instance/instance_wdt.h:
+
+/home/rui/SDKs/RIOT/cpu/samd21/include/pio/pio_samr21g18a.h:
+
+/home/rui/SDKs/RIOT/core/include/irq.h:
+
+/usr/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h:
+
+/home/rui/SDKs/RIOT/core/include/arch/irq_arch.h:
