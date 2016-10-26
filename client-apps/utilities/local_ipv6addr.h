120f12306efc5a744cba05132f5bcc0664b07f31
lightertu <ruitu.login@gmail.com>
2016-10-26 18:38:15 +0000

    get address done

--- client-apps/utilities/local_ipv6addr.h
+++ client-apps/utilities/local_ipv6addr.h
@@ -3,6 +3,8 @@
 
 #define LOWPAN 1 // 6lowpan interface
 #define ETH 0    // ethernet 
+#define LOCAL 1    // ethernet 
+#define GLOBAL 0    // ethernet 
 
 char * getaddr_if(int interface, int isLocalAddr);
 
