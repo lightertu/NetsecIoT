af450511596d534519f4b676628c34dcfd3a01c6
lightertu <ruitu.login@gmail.com>
2016-10-30 15:23:01 -0700

    temperature sensor

--- server-apps/sensor/include/testbed-hardware.h
+++ server-apps/sensor/include/testbed-hardware.h
@@ -11,7 +11,26 @@
 #define ON 1
 #define OFF 0
 
-uint32_t read_temperature(void);
+#define I2C_INTERFACE I2C_DEV(0)    /* I2C interface number */
+#define SENSOR_ADDR   (0x48 | 0x07) /* I2C temperature address on sensor */
+#define INTERVAL      (5000000U)    /* set interval to 5 seconds */
+
+uint32_t read_temperature_dummy(void);
 void led_switch(int);
+
+/*  to use the read temperature function first write these code
+    int init = i2c_init_master(I2C_INTERFACE, I2C_SPEED_NORMAL);
+    if (init == -1) {
+        puts("Error: Init: Given device not available\n");
+        return 1;
+    }
+    else if (init == -2) {
+        puts("Error: Init: Unsupported speed value\n");
+        return 1;
+    }
+    else {
+        printf("I2C interface %i successfully initialized as master!\n", I2C_INTERFACE);
+*/
+int read_temperature(void);
 
 #endif
--- server-apps/sensor/testbed-hardware.c
+++ server-apps/sensor/testbed-hardware.c
@@ -6,10 +6,12 @@
 #include <random.h>
 #include <led.h>
 #include <xtimer.h>
+#include "periph_conf.h"
+#include "periph/i2c.h"
 #include "./include/testbed-hardware.h"
 
 
-uint32_t read_temperature(void) {
+uint32_t read_temperature_dummy(void) {
     random_init(xtimer_now());
     uint32_t curtmp;
     curtmp = random_uint32_range(20, 30);
@@ -23,4 +25,28 @@ void led_switch(int on) {
     } else {
         LED0_OFF;
     }
+}
+
+
+int read_temperature(void) {
+    uint16_t temperature;
+    char buffer[2] = { 0 };
+    /* read temperature register on I2C bus */
+    if (i2c_read_bytes(I2C_INTERFACE, SENSOR_ADDR, buffer, 2) < 0) {
+	printf("Error: cannot read at address %i on I2C interface %i\n",
+	       SENSOR_ADDR, I2C_INTERFACE);
+	return -1;
+    }
+    
+    uint16_t data = (buffer[0] << 8) | buffer[1];
+    int8_t sign = 1;
+    /* Check if negative and clear sign bit. */
+    if (data & (1 << 15)) {
+	sign *= -1;
+	data &= ~(1 << 15);
+    }
+    /* Convert to temperature */
+    data = (data >> 5);
+    temperature = data * sign * 0.125;
+    return (int)temperature;
 }
