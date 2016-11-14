00d72a8a8d07b211ab12f63cbd3070007716d2fe
lightertu <ruitu.login@gmail.com>
2016-11-13 17:37:23 -0800

    new client for nonembedded

--- client-apps/Non-Embedded/coap-client/src/main/java/org/netsec/iottestbed/nonembedded/Client.java
+++ client-apps/Non-Embedded/coap-client/src/main/java/org/netsec/iottestbed/nonembedded/Client.java
@@ -1,78 +1,100 @@
 package org.netsec.iottestbed.nonembedded;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
-import org.eclipse.californium.core.CoapHandler;
-import org.eclipse.californium.core.CoapObserveRelation;
 import org.eclipse.californium.core.coap.MediaTypeRegistry;
+
+
+class TemperatureReader {
+    private CoapResponse _getResponse;
+    private CoapClient temperatureClient;
+    private String _uri;
+
+    TemperatureReader(String uri) {
+        _uri = uri;
+        temperatureClient = new CoapClient(uri);
+    }
+    String getTemperature() {
+        _getResponse = temperatureClient.get();
+        if (_getResponse.getCode().codeClass == 4) {
+            System.out.println("Temperature Request to " + _uri + " returnd code " + _getResponse.getCode());
+        }
+        return _getResponse.getResponseText();
+    }
+
+    public String lastResponse() {
+        return _getResponse.getResponseText();
+    }
+}
+
+
+
+class LEDcontroller {
+    private CoapResponse _getResponse;
+    private CoapClient _LEDclient;
+    private String _uri;
+
+    LEDcontroller(String uri) {
+        _uri = uri;
+        _LEDclient = new CoapClient(uri);
+    }
+
+    String on() {
+        _getResponse = _LEDclient.put("ON", MediaTypeRegistry.TEXT_PLAIN);
+        if (_getResponse.getCode().codeClass == 4) {
+            System.out.println("LEDcontroller ON to " + _uri + " returnd code " + _getResponse.getCode());
+        }
+        return _getResponse.getResponseText();
+    }
+
+    String off() {
+        _getResponse = _LEDclient.put("OFF", MediaTypeRegistry.TEXT_PLAIN);
+        if (_getResponse.getCode().codeClass == 4) {
+            System.out.println("LEDcontroller OFF to " + _uri + " returnd code " + _getResponse.getCode());
+        }
+        return _getResponse.getResponseText();
+    }
+
+    public String toggle() {
+        _getResponse = _LEDclient.put("TOGGLE", MediaTypeRegistry.TEXT_PLAIN);
+        if (_getResponse.getCode().codeClass == 4) {
+            System.out.println("LEDcontroller TOGGLE to " + _uri + " returnd code " + _getResponse.getCode());
+        }
+        return _getResponse.getResponseText();
+    }
+
+    public String lastResponse() {
+        return _getResponse.getResponseText();
+    }
+}
 
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        CoapClient client = new CoapClient("coap://californium.eclipse.org:5683/obs");
+        TemperatureReader temperatureClient = new TemperatureReader("coap://californium.eclipse.org:5683/obs");
+        LEDcontroller ledClient = new LEDcontroller("coap://californium.eclipse.org:5683/obs");
 
-        // synchronous
-        String content1 = client.get().getResponseText();
-        // CoapResponse putResponse = client.put("ON", MediaTypeRegistry.TEXT_PLAIN);
-        System.out.println("RESPONSE 1: " + content1);
-        CoapResponse resp2 = client.post("payload", MediaTypeRegistry.TEXT_PLAIN);
-        System.out.println("RESPONSE 2 CODE: " + resp2.getCode());
+        int ctr = 0;
 
-        // asynchronous
-
-        /*
-        System.out.println("ASYNCHRONOUS (press enter to continue)");
-
-        client.get(new CoapHandler() {
-            public void onLoad(CoapResponse response) {
-                String content = response.getResponseText();
-                System.out.println("RESPONSE 3: " + content);
+        while (true) {
+            String putResponse;
+            if (ctr == 0) {
+                putResponse = ledClient.on();
+            } else {
+                putResponse = ledClient.off();
             }
 
-            public void onError() {
-                System.err.println("FAILED");
+            ctr = ~ctr;
+            System.out.println("LED status: " + putResponse);
+            System.out.println("temperature: " + temperatureClient.getTemperature());
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-
-        // wait for user
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            br.readLine();
-        } catch (IOException e)
-        { }
-
-        */
-        // observe
-
-        /*
-        System.out.println("OBSERVE (press enter to exit)");
-
-        CoapObserveRelation relation = client.observe(
-                new CoapHandler() {
-                    public void onLoad(CoapResponse response) {
-                        String content = response.getResponseText();
-                        System.out.println("NOTIFICATION: " + content);
-                    }
-
-                    public void onError() {
-                        System.err.println("OBSERVING FAILED (press enter to exit)");
-                    }
-                });
-
-        // wait for user
-        try { br.readLine(); } catch (IOException e) { }
-        */
-
-        /*
-        System.out.println("CANCELLATION");
-
-        relation.proactiveCancel();
-        */
+        }
     }
 }
 
