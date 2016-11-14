3a42054e1f2f91905561416e8c53aa4c8e3039f7
lightertu <ruitu.login@gmail.com>
2016-11-13 23:19:06 -0800

    changes

--- client-apps/Non-Embedded/coap-client/src/main/java/org/netsec/iottestbed/nonembedded/Client.java
+++ client-apps/Non-Embedded/coap-client/src/main/java/org/netsec/iottestbed/nonembedded/Client.java
@@ -1,94 +1,42 @@
 package org.netsec.iottestbed.nonembedded;
 
-import org.eclipse.californium.core.CoapClient;
-import org.eclipse.californium.core.CoapResponse;
-import org.eclipse.californium.core.coap.MediaTypeRegistry;
-
-
-class TemperatureReader {
-    private CoapResponse _getResponse;
-    private CoapClient temperatureClient;
-    private String _uri;
-
-    TemperatureReader(String uri) {
-        _uri = uri;
-        temperatureClient = new CoapClient(uri);
-    }
-    String getTemperature() {
-        _getResponse = temperatureClient.get();
-        if (_getResponse.getCode().codeClass == 4) {
-            System.out.println("Temperature Request to " + _uri + " returnd code " + _getResponse.getCode());
-        }
-        return _getResponse.getResponseText();
-    }
-
-    public String lastResponse() {
-        return _getResponse.getResponseText();
+class TemperatureReader extends SensorReader {
+    TemperatureReader(String address) {
+        super(address, "temperature");
     }
 }
 
-
-
-class LEDcontroller {
-    private CoapResponse _getResponse;
-    private CoapClient _LEDclient;
-    private String _uri;
-
-    LEDcontroller(String uri) {
-        _uri = uri;
-        _LEDclient = new CoapClient(uri);
+class LEDController extends ActuatorController {
+    LEDController(String addr) {
+        super(addr, "led");
     }
 
-    String on() {
-        _getResponse = _LEDclient.put("ON", MediaTypeRegistry.TEXT_PLAIN);
-        if (_getResponse.getCode().codeClass == 4) {
-            System.out.println("LEDcontroller ON to " + _uri + " returnd code " + _getResponse.getCode());
-        }
-        return _getResponse.getResponseText();
+    String on() throws InterruptedException {
+       return sendCommand("ON");
     }
 
-    String off() {
-        _getResponse = _LEDclient.put("OFF", MediaTypeRegistry.TEXT_PLAIN);
-        if (_getResponse.getCode().codeClass == 4) {
-            System.out.println("LEDcontroller OFF to " + _uri + " returnd code " + _getResponse.getCode());
-        }
-        return _getResponse.getResponseText();
-    }
-
-    public String toggle() {
-        _getResponse = _LEDclient.put("TOGGLE", MediaTypeRegistry.TEXT_PLAIN);
-        if (_getResponse.getCode().codeClass == 4) {
-            System.out.println("LEDcontroller TOGGLE to " + _uri + " returnd code " + _getResponse.getCode());
-        }
-        return _getResponse.getResponseText();
-    }
-
-    public String lastResponse() {
-        return _getResponse.getResponseText();
+    String off() throws InterruptedException {
+        return sendCommand("OFF");
     }
 }
 
 public class Client {
-
     public static void main(String[] args) throws InterruptedException {
-
-        TemperatureReader temperatureClient = new TemperatureReader("coap://californium.eclipse.org:5683/obs");
-        LEDcontroller ledClient = new LEDcontroller("coap://californium.eclipse.org:5683/obs");
-
-        int ctr = 0,
-            time = 0;
-
+        String boardAddress = "[fe80::5844:2342:656a:f846]";
+        TemperatureReader temperatureReader = new TemperatureReader(boardAddress);
+        LEDController ledController = new LEDController(boardAddress);
+        int ctr = 0, time = 0;
         while (time++ < 20) {
             String putResponse;
             if (ctr == 0) {
-                putResponse = ledClient.on();
+                putResponse = ledController.on();
             } else {
-                putResponse = ledClient.off();
+                putResponse = ledController.off();
             }
 
             ctr = ~ctr;
             System.out.println("LED status: " + putResponse);
-            System.out.println("temperature: " + temperatureClient.getTemperature());
+            System.out.println("temperature: " + temperatureReader.read());
 
             try {
                 Thread.sleep(3000);
@@ -98,4 +46,3 @@ public class Client {
         }
     }
 }
-
