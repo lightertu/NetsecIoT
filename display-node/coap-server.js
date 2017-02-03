3586b333b9e721f385d66778aeb3d45bf5739111
lightertu <ruitu.login@gmail.com>
2017-02-03 22:57:44 +0000

    changed fixed the bug in coap server

--- display-node/coap-server.js
+++ display-node/coap-server.js
@@ -2,43 +2,43 @@ var coap           = require('coap');
 var HashMap        = require('hashmap');
 var coapDevicesMap = new HashMap();
 var server         = coap.createServer({ type: 'udp6' });
+
 var coapDevicesMap = new HashMap();
+
+function coapServicesStringParser(serviceString) {
+    var raw_services = serviceString.split(",");
+
+    var services = [];
+    raw_services.pop();
+
+    for (var i = 0; i < raw_services.length; i++) {
+        var s = raw_services[i].split("|");
+        var newService = {
+            path: s[0],
+            method: s[1]
+        };
+        services.push(newService);
+    }
+
+    return services;
+}
 
 module.exports = {
     server: server,
-
     coapDevicesMap: coapDevicesMap,
-
-    coapServicesStringParser: function (serviceString) {
-        var raw_services = serviceString.split(",");
-        var services = [];
-        raw_services.pop();
-        for (var i = 0; i < raw_services.length; i++) {
-            var s = raw_services[i].split("|");
-            var newService = {
-                path: s[0],
-                method: s[1]
-            };
-            services.push(newService);
-        }
-
-        return services;
-    }
 }
 
 server.on('request', function(req, res) {
-    if (req.url === "/devices/nodes") {
-        var deviceAddress = req.rsinfo.address;
+    var deviceAddress = req.rsinfo.address;
+    if (req.url == "/devices/nodes") {
         if (!coapDevicesMap.has(deviceAddress)) {
             console.log("Found new node: " + deviceAddress);
-            var servicesArray = coapServicesStringParser(req.payload.toString('ascii'));
+            var serviceString = req.payload.toString('ascii');
+            var servicesArray = coapServicesStringParser(serviceString);
             coapDevicesMap.set(deviceAddress, servicesArray);
             console.log(servicesArray);
         }
-
-    } else {
-        console.log("Doesn't have endpoint: " + req.url);
-    }
+    } 
 });
 
 server.listen(function() {
