37a032293065950e35017725cd4b1265a1c0952f
lightertu <ruitu.login@gmail.com>
2017-02-11 12:35:55 -0800

    added an icon

--- display-node/public/app/src/components/PageHeader.js
+++ display-node/public/app/src/components/PageHeader.js
@@ -2,6 +2,13 @@
  * Created by rui on 2/4/17.
  */
 import React from "react"
+import FontIcon from 'material-ui/FontIcon';
+const iconStyles = {
+    marginRight: 10,
+    fontSize: '48px',
+};
+
+
 export default class DeviceGalleryHeader extends React.Component {
     constructor(){
         super();
@@ -10,7 +17,8 @@ export default class DeviceGalleryHeader extends React.Component {
         return (
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header"> { this.props.name }
+                    <h1 class="page-header">
+                        <FontIcon className="material-icons" style={iconStyles}> developer_board </FontIcon>
                         <small> { this.props.description }</small>
                     </h1>
                 </div>
