42ff18415a434841de8543981075e9b5944bf06b
lightertu <ruitu.login@gmail.com>
2017-02-21 17:26:41 -0800

    somechanges

--- display-node/server/coap/coapClientDebug.js
+++ display-node/server/coap/coapClientDebug.js
@@ -1,3 +1,19 @@
 /**
  * Created by rui on 2/21/17.
  */
+let coap             = require('coap');
+
+let req = coap.request('coap://[ff05::fd]/.well-known/core');
+
+req.on('response', function(res) {
+    res.pipe(process.stdout);
+    res.on('end', function() {
+        process.exit(0)
+    })
+
+    res.on('error', function(error){
+       console.log(error);
+    });
+});
+
+req.end();
--- rpi-node/src/main/java/org/netsec/iottestbed/nonembedded/Server.java
+++ rpi-node/src/main/java/org/netsec/iottestbed/nonembedded/Server.java
@@ -43,7 +43,7 @@ class AdvertisingRunnable implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(_adString);
+            //System.out.println(_adString);
             _advertisingClient.put(_adString, 0);
         }
     }
